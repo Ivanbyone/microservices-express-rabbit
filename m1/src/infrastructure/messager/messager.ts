5c7384231297397d0c8a27108fe85dad3ab8e9b1
ivanbyone <ivanbyone@gmail.com>
2025-01-30 15:33:46 +0300

    Updated m1 (added waiting response from queue)

--- m1/src/infrastructure/messager/messager.ts
+++ m1/src/infrastructure/messager/messager.ts
@@ -5,14 +5,14 @@ import { TYPES } from "../../types/types";
 
 export interface IRabbitService {
     connect(): Promise<void>;
-    sendMessage(message: string): Promise<void>;
+    sendMessageAndListen(message: string): Promise<string>;
 }
 
 @injectable()
 export class RabbitService implements IRabbitService {
     private readonly _logger: ILogger;
-    private connection: amqp.connection;
-    private channel: amqp.channel;
+    private connection: amqp.Connection;
+    private channel: amqp.Channel;
 
     private readonly rabbitMQUrl: string = process.env.RABBITMQ_URL;
 
@@ -29,18 +29,36 @@ export class RabbitService implements IRabbitService {
         }
     }
 
-    async sendMessage(message: string): Promise<void> {
+    async sendMessageAndListen(message: string): Promise<string> {
+        await this.connect();
+
         if (!this.connection || !this.channel) {
             await this.connect();
             this._logger.get().error("Channel isn't init");
         }
 
-        await this.channel.assertQueue(process.env.QUEUE, { durable: true });
+        await this.channel.assertQueue(process.env.QUEUE, { durable: false });
 
-        await this.channel.sendToQueue(
-            process.env.QUEUE,
-            Buffer.from(message),
-            { persistent: true },
-        );
+        const reply = await this.channel.assertQueue("", { exclusive: true });
+        const correlationId = crypto.randomUUID();
+
+        this.channel.sendToQueue(process.env.QUEUE, Buffer.from(message), {
+            correlationId: correlationId,
+            replyTo: reply.queue,
+        });
+
+        return new Promise((response) => {
+            this.channel.consume(reply.queue, async (message) => {
+                if (message.properties.correlationId === correlationId) {
+                    if (!message) {
+                        return null;
+                    } else {
+                        response(message.content.toString());
+                        await this.channel.close();
+                        await this.connection.close();
+                    }
+                }
+            });
+        });
     }
 }
