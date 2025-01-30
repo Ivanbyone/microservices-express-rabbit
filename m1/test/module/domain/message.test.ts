e92f357617a0b3add904ee0d66cdeef702d211f5
ivanbyone <ivanbyone@gmail.com>
2025-01-30 11:07:22 +0300

    Updated ignore for prettier

--- m1/test/module/domain/message.test.ts
+++ m1/test/module/domain/message.test.ts
@@ -1,9 +1,9 @@
 import { describe, test, expect } from "@jest/globals";
-import { Message } from '../../../src/domain/Message';
+import { Message } from "../../../src/domain/Message";
 
-describe('Creating Message object', () => {
-    test('Valid Message object', () => {
+describe("Creating Message object", () => {
+    test("Valid Message object", () => {
         const obj = new Message(8);
         expect(obj).toEqual({ value: 8 });
-    })
+    });
 });
